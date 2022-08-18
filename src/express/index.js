c92b54a6142bdcd4e29441a2729841d08008dfa4
Ali Azmoodeh <treeroot.ir@gmail.com>
2022-08-18 20:50:26 +0430

    added ui config files

--- src/express/index.js
+++ src/express/index.js
@@ -2,5 +2,5 @@ let lib = require('../../lib/index');
 
 
 module.exports = (expressApp, route) => {
-    expressApp?.get(route === undefined ? '/socket-docs' : `/${route}`, lib.reqHandler());
+    expressApp?.get(route === undefined ? '/socket-docs' : `/${route}`, lib.reqHandler);
 }
